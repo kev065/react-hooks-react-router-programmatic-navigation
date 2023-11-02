8366c3bbce3876a739b5194cbe4d9ce4deb2f2ba
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-02 16:10:57 +0300

    completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 function Home({ isLoggedIn }) {
+
+  // if the user isn't logged in, redirect them to the login page
   if (!isLoggedIn) return <Redirect to="/login" />;
 
+  // otherwise, return the home page
   return (
     <div>
-      <h1></h1>
+      <h1>Welcome, my boy</h1>
     </div>
   );
 }
